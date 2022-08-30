1c2b6e25d1ceeeeb647551043caf2ac37ff41fd2
pioustu <3337808342@qq.com>
2022-08-30 08:00:02 +0800

    5 fold to 4 fold

--- split_KFold.py
+++ split_KFold.py
@@ -20,10 +20,11 @@ test_data_client_all = [torch.load(p) for p in test_data_client_path]
 
 set_seed()
 
+KK = 4
 id = 1
 for train,val,test in zip(train_data_client_all,val_data_client_all,test_data_client_all):
     dataset = train+val 
-    if id <=8: # 分类正负样本分别划分，之后在进行拼接保证5Fold正负样本对称
+    if id <=8: # 分类正负样本分别划分，之后在进行拼接保证KKFold正负样本对称
         P = [data for data in dataset if data.y==1]
         N = [data for data in dataset if data.y==0]
         print("Client:{}, 正样本数:{}, 负样本数:{}".format(id, len(P), len(N)))
@@ -31,11 +32,11 @@ for train,val,test in zip(train_data_client_all,val_data_client_all,test_data_cl
         N_N = len(N)
         P_idxs = list(range(P_N))
         N_idxs = list(range(N_N))
-        P_bin = math.ceil(P_N/5)
-        N_bin = math.ceil(N_N/5)
+        P_bin = math.ceil(P_N/KK)
+        N_bin = math.ceil(N_N/KK)
         Fold_P = []
         Fold_N = []
-        for i in range(4): # 首先获取四个，最后一个用于补齐
+        for i in range(KK-1): # 首先获取四个，最后一个用于补齐
             tmp = []
             P_rdm = np.random.choice(P_idxs,P_bin,replace=False)
             N_rdm = np.random.choice(N_idxs,N_bin,replace=False)
@@ -54,9 +55,9 @@ for train,val,test in zip(train_data_client_all,val_data_client_all,test_data_cl
     else: #回归直接均匀划分即可
         N = len(dataset)
         T_idxs = list(range(N))
-        T_bin = math.ceil(N/5)
+        T_bin = math.ceil(N/KK)
         Fold = []
-        for i in range(4):
+        for i in range(KK-1):
             tmp = []
             rdm = np.random.choice(T_idxs,T_bin,replace=False)
             tmp = [dataset[k] for k in rdm]
@@ -72,7 +73,7 @@ for train,val,test in zip(train_data_client_all,val_data_client_all,test_data_cl
     # 分别保存四个划分结果，并且对data_index进行重新初始化,并且保存到相应的位置
     for k in range(len(Fold)):
         F_val = Fold[k]
-        F_train = Fold[(k+1)%5] + Fold[(k+2)%5] + Fold[(k+3)%5] + Fold[(k+4)%5]
+        F_train = Fold[(k+1)%KK] + Fold[(k+2)%KK] + Fold[(k+3)%KK] + Fold[(k+4)%KK]
         # 打乱
         np.random.shuffle(F_train)
         np.random.shuffle(F_val)
@@ -94,4 +95,4 @@ for train,val,test in zip(train_data_client_all,val_data_client_all,test_data_cl
         torch.save(test,path_test)
 
     id = id + 1
-    # print(len(P),len(N),len(P)//5,len(N)//5)
+    # print(len(P),len(N),len(P)//KK,len(N)//KK)
